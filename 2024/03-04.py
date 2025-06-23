fdb4c151fb25af4bd277555458709cb39f86dc85
Paul Foster <paulfstr@gmail.com>
2025-06-23 16:16:44 +1000

    Complete 2024-03-04

---
+++ 2024/03-04.py
@@ -0,0 +1,19 @@
+import collections
+
+with open("./2024/input/03-04.txt") as f:
+    lines = f.read().splitlines()
+
+prices = collections.defaultdict(int)
+for line in lines:
+    name, fee_type, price = line.split()
+    name = name[:-1]
+    price = int(price)
+
+    assert fee_type in ["Seat", "Meals", "Luggage", "Fee", "Tax", "Discount", "Rebate"]
+    if fee_type in ["Discount", "Rebate"]:
+        price = -price
+
+    prices[name] += price
+
+answer1 = min(prices.values())
+print(answer1)
