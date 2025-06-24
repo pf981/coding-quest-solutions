8f2bb5bcd3c26332c2b1c493e925be0533579da9
Paul Foster <paulfstr@gmail.com>
2025-06-24 23:00:30 +1000

    Simplify solution

--- 2022/05-05.py
+++ 2022/05-05.py
@@ -4,6 +4,27 @@ import math
 import downloader
 
 
+def has_line(x: int, y: int, dy: int) -> bool:
+    player = cols[x][y]
+    streak = 1
+
+    x2 = x - 1
+    y2 = y - dy
+    while x2 >= 0 and y2 >= 0 and len(cols[x2]) > y2 and cols[x2][y2] == player:
+        streak += 1
+        x2 -= 1
+        y2 -= dy
+
+    x2 = x + 1
+    y2 = y + dy
+    while x2 < 7 and y2 >= 0 and len(cols[x2]) > y2 and cols[x2][y2] == player:
+        streak += 1
+        x2 += 1
+        y2 += dy
+
+    return streak >= 4
+
+
 def has_won(cols: list[list[int]], play: int) -> bool:
     player = cols[play][-1]
     y = len(cols[play]) - 1
@@ -13,56 +34,15 @@ def has_won(cols: list[list[int]], play: int) -> bool:
         return True
 
     # Horizontal
-    streak = 1
-    i = play - 1
-    while i >= 0 and len(cols[i]) > y and cols[i][y] == player:
-        streak += 1
-        i -= 1
-
-    i = play + 1
-    while i < 7 and len(cols[i]) > y and cols[i][y] == player:
-        streak += 1
-        i += 1
-
-    if streak >= 4:
+    if has_line(play, y, 0):
         return True
 
-    # Descending Diagonal
-    streak = 1
-    i = play - 1
-    y2 = y + 1
-    while i >= 0 and len(cols[i]) > y2 and cols[i][y2] == player:
-        streak += 1
-        y2 += 1
-        i -= 1
-
-    i = play + 1
-    y2 = y - 1
-    while i < 7 and y2 >= 0 and len(cols[i]) > y2 and cols[i][y2] == player:
-        streak += 1
-        y2 -= 1
-        i += 1
-
-    if streak >= 4:
+    # Diagonal 1
+    if has_line(play, y, 1):
         return True
 
-    # Ascending Diagonal
-    streak = 1
-    i = play - 1
-    y2 = y - 1
-    while i >= 0 and y2 >= 0 and len(cols[i]) > y2 and cols[i][y2] == player:
-        streak += 1
-        y2 -= 1
-        i -= 1
-
-    i = play + 1
-    y2 = y + 1
-    while i < 7 and len(cols[i]) > y2 and cols[i][y2] == player:
-        streak += 1
-        y2 += 1
-        i += 1
-
-    if streak >= 4:
+    # Diagonal 2
+    if has_line(play, y, -1):
         return True
 
     return False
