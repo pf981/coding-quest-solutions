2270b9ffdc2022eabfe96cc62f48fba97d91440b
Paul Foster <paulfstr@gmail.com>
2025-06-24 20:45:36 +1000

    Use memoisation

--- 2023/03-15.py
+++ 2023/03-15.py
@@ -1,19 +1,28 @@
-import itertools
+import functools
 
 import downloader
 
 lines = downloader.get_puzzle(25).splitlines()
-
 n = len(lines)
 m = [[int(s) for s in line.split()] for line in lines]
 
-answer = float("inf")
-for path in itertools.permutations(range(1, n), n - 1):
-    i = d = 0
-    for j in path:
-        d += m[i][j]
-        i = j
-    d += m[i][0]
-    answer = min(answer, d)
 
+@functools.cache
+def tsp(pos, visited_mask):
+    if visited_mask == (1 << n) - 1:
+        return m[pos][0]
+
+    min_cost = float("inf")
+    for next_city in range(n):
+        if visited_mask & (1 << next_city):
+            continue
+
+        new_mask = visited_mask | (1 << next_city)
+        cost = m[pos][next_city] + tsp(next_city, new_mask)
+        min_cost = min(min_cost, cost)
+
+    return min_cost
+
+
+answer = tsp(0, 1)
 print(answer)
